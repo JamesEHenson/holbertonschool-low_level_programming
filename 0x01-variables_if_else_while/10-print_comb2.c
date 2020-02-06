aabe7abf56198cfded6675b4dddf382a58ea556c
JamesEHenson <spaznaught50@gmail.com>
2020-02-06 20:58:42 +0000

    finished last one

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+  int i;
+
+  int u;
+
+
+
+  for (i = 48; i <= 57; i++)
+
+    {
+
+      for (u = 48; u <= 57; u++)
+
+	{
+
+	  putchar(i);
+
+	  putchar(u);
+
+	  if (i != 57 || u != 57)
+
+	    {
+
+	      putchar(',');
+
+	      putchar(' ');
+
+	    }
+
+	}
+
+    }
+
+  putchar('\n');
+
+  return (0);
+
+}
