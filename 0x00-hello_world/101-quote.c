3976c6a82a8ce30c128a05411a8cd8adb459b97e
JamesEHenson <spaznaught50@gmail.com>
2020-02-11 20:23:27 +0000

    fixed code to betty standard

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+
+
 
 int main(void)
 {
 int fd;
-char Buff[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+char Buff[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19/n";
 fd = 1;
 write(fd, Buff, 60);
 return (1);
