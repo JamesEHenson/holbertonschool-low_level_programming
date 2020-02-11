b8799727b011e118adfb5507fc0effc7b79184db
JamesEHenson <spaznaught50@gmail.com>
2020-02-11 20:17:02 +0000

    fixed 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 int main(void)
 {
 int fd;
-char Buff[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char Buff[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
 fd = 1;
 write(fd, Buff, 60);
 return (1);
