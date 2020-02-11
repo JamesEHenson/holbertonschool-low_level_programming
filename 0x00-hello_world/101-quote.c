f38737bce2043d7e772ca872280d27fa2f20dc24
JamesEHenson <spaznaught50@gmail.com>
2020-02-11 20:24:51 +0000

    fixed added characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 int fd;
-char Buff[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19/n";
+char Buff[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19";
 fd = 1;
 write(fd, Buff, 60);
 return (1);
