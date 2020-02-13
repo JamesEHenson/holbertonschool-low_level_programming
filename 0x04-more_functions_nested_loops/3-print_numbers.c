d76f74dee5996d600b0097251c7ef3e692bdda89
JamesEHenson <spaznaught50@gmail.com>
2020-02-13 19:26:44 +0000

    finished 2-4 with code

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
-3
+#include <holberton.h>
+
+/**
+ *print_numbers - checks for digit(0 - 9)
+ *Return: Always 0 (success)
+ */
+
+void print_numbers(void);
+{
+int a;
+
+for (a= 48 a < 58; a++)
+{
+  _putchar(a);
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,19 @@
-4
+#include <holberton.h>
+
+
+/**
+ *print_most_numbers-print numbers between 0-9 except 2,4
+ *
+ *Return: 0 (success)
+ *
+ */
+
+void print_most_numbers(void)
+{
+  int a;
+  if (a=>48 a=<57 !(50,52));
+  {
+    _putchar(a);
+  }
+  _putchar('\n');
+}
