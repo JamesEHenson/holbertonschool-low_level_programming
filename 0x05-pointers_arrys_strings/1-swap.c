7abe247954634e0be01732d30094723750e76d7b
JamesEHenson <spaznaught50@gmail.com>
2020-02-18 14:23:10 +0000

    finishing main objectives

--- 0x05-pointers_arrys_strings/1-swap.c
+++ 0x05-pointers_arrys_strings/1-swap.c
@@ -1,1 +1,15 @@
-1
+#include "holberton.h"
+
+/**
+ * swap_int - swap the value of a and b.
+ * @a : a is a value
+ * @b : b is a value
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
