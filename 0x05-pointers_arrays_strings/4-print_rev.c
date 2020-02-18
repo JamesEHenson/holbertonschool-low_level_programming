152e60ceface8cbf678e5105c8a5cb05a658ca5c
JamesEHenson <spaznaught50@gmail.com>
2020-02-18 14:44:53 +0000

    fixing things for betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *s)
 {
-  int i = 0;
+int i = 0;
 
-  while (s[i] != '\0')
-    {
-      i++;
-    }
-  i--;
-  for (; i >= 0; i--)
-    {
-      _putchar(s[i]);
-    }
-  _putchar('\n');
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+for (; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
