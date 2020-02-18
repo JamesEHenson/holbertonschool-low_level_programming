5accfd9a88518ea869dbd3783eaea03df09cbe13
JamesEHenson <spaznaught50@gmail.com>
2020-02-18 14:36:40 +0000

    fixing code for betty

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
  * rev_string - print reverse characters.
@@ -7,18 +7,18 @@
  */
 void rev_string(char *s)
 {
-  int i = 0, a = 0, Aux;
+int i = 0, a = 0, Aux;
 
 
-  while (s[i] != '\0')
-    {
-      i++;
-    }
-  i--;
-  while (i > a)
-    {
-      Aux = s[i];
-      s[i--] = s[a];
-      s[a++] = Aux;
-    }
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+while (i > a)
+{
+Aux = s[i];
+s[i--] = s[a];
+s[a++] = Aux;
+}
 }
