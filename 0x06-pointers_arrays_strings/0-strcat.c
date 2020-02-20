0e8ec35f87c331b0ff8ab8a106959ffcc6121bc4
JamesEHenson <spaznaught50@gmail.com>
2020-02-20 16:04:19 +0000

    adding holberton.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,15 @@
-1
+#include "holberton.h"
+
+
+char *_strcat(char *dest, char *src);
+int size; 
+
+{
+  size dest_len = strlen(dest);
+  size i;
+
+  for (i=0; i<n && src[i] != '\0'; i++)
+    dest[dest_len + i]= src[i];
+  dest[dest_len + i] = '\0';
+  return dest;
+}
