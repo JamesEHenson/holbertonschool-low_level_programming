fe1976954c8af23821b1a73b8ee767c6f3019474
JamesEHenson <spaznaught50@gmail.com>
2020-02-21 03:46:28 +0000

    corrupted data

---
+++ Test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main entry
+ *
+ *Description: print out grammer exact
+ *
+ */
+int main (void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
