9ca7ed78d48b6529a1b5601013eb52ec6d3a748b
JamesEHenson <55972025+JamesEHenson@users.noreply.github.com>
2020-02-20 22:05:35 -0600

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * rot13 - Function that reverses the content of an array of integers.
  *
@@ -12,7 +11,6 @@ char *rot13(char *str)
 int i = 0, k;
 char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
 for (; str[i] != '\0'; i++)
 {
 for (k = 0; k <= 51; k++)
