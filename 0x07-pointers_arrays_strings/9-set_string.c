00fce39042e784613ef6456084c5392afc7abad3
JamesEHenson <spaznaught50@gmail.com>
2020-02-24 23:05:03 +0000

    finished

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+
+/**
+ * set_string - prints buffer in hexa
+ * @s: tried to hard
+ * @to: buffer2
+ *
+ * Return: Nothing.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
