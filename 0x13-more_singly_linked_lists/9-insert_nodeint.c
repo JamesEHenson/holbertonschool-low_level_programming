4d539383a2c186d138cc331c2bdf50aaaa74f6c0
JamesEHenson <Spaznaught50@gmail.com>
2020-03-25 10:15:22 +0000

    fixing betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,11 @@
  * @n: value of number.
  * Return: Value of a node index.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
 {
+
 unsigned int i = 1;
 listint_t *index_node = *head;
 listint_t *insert;
@@ -20,6 +23,7 @@ listint_t *h = *head;
 if (head == NULL)
 return (NULL);
 while (i < idx)
+
 {
 index_node = (*head)->next;
 *head = index_node;
